4a91d118d3a5ed47b9221f37813504b39e0cdcb1
magic-9 <zizo.peter@yahoo.com>
2020-02-22 14:55:41 +0200

    add card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:nettuto/main.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,6 +11,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   bool is_loading = false;
+  List<String> quotes = [
+    'By yourself',
+    "don't do this here",
+    "i'm not vagetables"
+  ];
 
   Widget getContent(){
      if(is_loading == false){
@@ -31,23 +37,101 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       appBar: AppBar(
         title: Text("App"),
         centerTitle: true,
         elevation: 1.0,
-        backgroundColor: Colors.deepOrangeAccent,
+        backgroundColor: Colors.grey[900],
       ),
-      body:ListView(
+      body: ListView(
         children: <Widget>[
-          getContent(),
-          RaisedButton(
-            onPressed: (){
-              setState(() {
-                is_loading =! is_loading;
-              });
-            },
+          Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 40, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+               Center(
+                 child:  CircleAvatar(
+                   backgroundImage: AssetImage(
+                       "images/peter.jpg"
+                   ),
+                   maxRadius: 100,
+                 ),
+               ),
+                Divider(
+                   color: Colors.amber,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Name",
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 2.0
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Peter Ramsis Helal",
+                  style: TextStyle(
+                      fontSize: 45,
+                      color: Colors.amber,
+                      fontFamily: "Sacramento"
+                  ),
+                ),
+                Text(
+                  "Job",
+                  style: TextStyle(
+                      color: Colors.white,
+                      letterSpacing: 2.0
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "full stack web developer",
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.amber,
+                      fontFamily: "Sacramento",
+                      letterSpacing: 2
+                  ),
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                        size: 25,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "peterramsis@ymail.com",
+                      style: TextStyle(
+                          fontSize: 30,
+                          letterSpacing: 2.0,
+                          color: Colors.amber,
+                          fontFamily: "Sacramento"
+                      ),
+                    )
+                  ],
+                ),
+
+              ],
+            ),
           )
-        ],
+        ] ,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
